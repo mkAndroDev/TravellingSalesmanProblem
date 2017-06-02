ce6b461ea7377c445289ac9126d3c9857b1e248c
Maciej Krawczyk <mkandrodev@gmail.com>
2017-06-03 01:42:08 +0200

    Added matrix to calculate route

---
+++ app/src/main/java/com/krawczyk/maciej/travellingsalesmanproblem/domain/ApiConfiguration.java
@@ -0,0 +1,8 @@
+package com.krawczyk.maciej.travellingsalesmanproblem.domain;
+
+/**
+ * Created by maciejkrawczyk on 03.06.2017.
+ */
+
+public class ApiConfigutration {
+}
---
+++ app/src/main/java/com/krawczyk/maciej/travellingsalesmanproblem/domain/models/Element.java
@@ -0,0 +1,8 @@
+package com.krawczyk.maciej.travellingsalesmanproblem.domain.models;
+
+/**
+ * Created by maciejkrawczyk on 03.06.2017.
+ */
+
+public class Element {
+}
